e4cad51989a75aa00ac4993348331b9ecefcc6b6
ah <ah@tuf>
2021-11-15 12:35:59 -0600

    initial demo version

---
+++ src/main.rs
@@ -0,0 +1,236 @@
+//#![windows_subsystem="windows"]
+mod win;
+
+use std::{cmp::{max, min}, collections::hash_map::DefaultHasher, default::Default, hash::{Hasher, SipHasher}, os::windows::thread, sync::{Arc, atomic::Ordering, mpsc::{TryRecvError, TrySendError}}, time::{Duration, Instant}};
+
+use winit::{dpi::PhysicalPosition, event::{Event, WindowEvent}, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder};
+
+use pixels::{Error, Pixels, SurfaceTexture};
+
+use screenshot;
+
+fn main() -> Result<(), Error> {
+    // screenshot::init();
+
+    let show_decoration = std::env::var("SHADES_NO_WIN_DECORATION").as_deref() != Ok("1");
+    let always_on_top = std::env::var("SHADES_NO_ALWAYS_ON_TOP").as_deref() != Ok("1");
+    let perf_mode = std::env::var("SHADES_PERF_MODE").as_deref() == Ok("1");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Shades")
+        .with_visible(false)
+        .with_decorations(show_decoration)
+        .with_always_on_top(always_on_top) 
+        .build(&event_loop)
+        .expect("Could not build window");
+
+    let id = window.id();
+
+    // std::thread::spawn(move || {
+    //     //let res = picker::pick_picker(&window);
+    //     //res.expect("Could not pick window");
+    //     //window
+        
+    // });
+
+    win::hide_from_capture(&window).expect("could not hide window from capture");
+
+
+    let (pix_sender, pix_receiver) = std::sync::mpsc::sync_channel(1);
+    // TODO: create capture thread
+    let window = Arc::new(window);
+    let winref = window.clone();
+    let pix_src = std::thread::spawn(move ||
+    {
+        std::thread::sleep( Duration::from_millis(500));
+        let recorder = screenshot::ScreenRecorder::capture_primary().expect("could not capture primary");
+
+        let mut last_hash = 0;
+        let mut hasher: DefaultHasher = Default::default();
+        loop {
+            let pix = recorder.next().expect("could  not take screenshot");
+            hasher.write(pix.data.as_ref());
+            let hash = hasher.finish();
+            if hash != last_hash {
+                winref.request_redraw();
+                match pix_sender.try_send(pix) {
+                    Err(TrySendError::Disconnected(_)) => break,
+                    Err(TrySendError::Full(_)) => (),
+                    Ok(_) => ()
+                };
+ 
+            } else {
+                if !perf_mode {
+                    std::thread::sleep(Duration::from_millis(30));
+                }
+            }
+            hasher = Default::default();
+            last_hash = hash;
+
+            // 
+            
+        }
+    });
+    
+    // let pix = screenshot::capture().expect("could not get screenshot");
+    // let mut pix = screenshot::capture_win(&window).expect("could not get screenshot");
+    let mut pix = pix_receiver.recv().unwrap();
+    println!("found {} x {} pixels", pix.width, pix.height);
+
+
+    let mut pixels = {
+        let window_size = window.as_ref().inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
+        Pixels::new(window_size.width, window_size.height, surface_texture)?
+    };
+    
+    // var pos = window.inner_position().unwrap();
+    window.request_redraw();
+
+    
+    window.set_visible(true);
+
+    let mut cnt = 0;
+    let mut dir = 1;
+
+    let mut last_hash = 0;
+    let mut hasher: DefaultHasher = Default::default();
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        if let Event::RedrawRequested(_) = event {
+            let pix = &mut pix;
+            let pix_opt = {
+                // drain channel
+                let mut tmp = None;
+                loop {
+                    // drain
+                    let trypix = pix_receiver.try_recv();
+                    if trypix.is_ok() {
+                        *pix = trypix.unwrap();
+                        tmp = Some(&pix)
+                    // }
+                    } else {
+                        break;
+                    }
+                }
+                tmp
+            }; 
+            if let Some(pix) = pix_opt {
+
+                // let offset_x = 0;
+                let target_width = window.inner_size().width as usize;
+                let target_height = window.inner_size().width as usize;
+                if target_width <= 0 || target_height <= 0 {
+                    return
+                }
+                let src_width = pix.width;
+                let src_height = pix.width as i32;
+                let mut offset_x = 0;
+                let mut offset_y = 0;
+                if let Ok(pos) = window.inner_position() {
+                    offset_x = pos.x;
+                    offset_y = pos.y;
+                }
+                let max_j = (pix.width*pix.height - 1) as i32;
+                // println!("found {} x {} pixels", pix.width, pix.height);
+                for (i, pixel) in pixels.get_frame().chunks_exact_mut(4).enumerate() {
+                    let col = (i % target_height) as i32;
+                    let row = (i / target_height) as i32; 
+                    let j = max(0, min(max_j, (row + offset_y)*src_height + col + offset_x)) as usize;
+                    pixel[2] = 255 - pix.data[j*4];
+                    pixel[1] = 255 - pix.data[j*4+1];
+                    pixel[0] = 255 - pix.data[j*4+2];
+                    hasher.write(pixel);
+                    // pixel[0] = 255-pix.data[j*4];
+                    // pixel[1] = 255-pix.data[j*4+1];
+                    // pixel[2] = 255-pix.data[j*4+2];
+                }
+                let frame = pixels.get_frame();
+                let flash = (cnt % 16)<<4;
+                for j in 0..10 {
+                    for i in 0..10 {
+                        // TODO: ensure small windows do not cause panic
+                        let k = (i+j*target_width as usize)*4;
+                        frame[k] = flash ^ frame[k];  
+                        // frame[k+1] = flash;  
+                        // frame[k+2] = flash;
+                    }
+                }
+                if dir == 1 { 
+                    cnt += 1;
+                } else {
+                    cnt -= 1;
+                }
+                if cnt == 0 || cnt == 15 { dir = -dir };
+                let hash = hasher.finish();
+                if last_hash != hash && pixels
+                    .render()
+                    .map_err(|e| panic!("pixels.render() failed: {:?}", e))
+                    .is_err()
+                {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                last_hash = hash;
+                hasher = Default::default();
+            }
+        }
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == id => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                window_id,
+            } if window_id == id && size.width > 0 && size.height > 0 => {
+                println!("resized to {:?}!", size);
+
+                pixels.resize_surface(size.width, size.height);
+                pixels.resize_buffer(size.width, size.height);
+                window.request_redraw();
+            }
+            _ => (),
+        }
+
+        // window.request_redraw();
+    });
+
+    #[allow(unreachable_code)]
+    {
+        pix_src.join();
+        Ok(())
+    }
+}
+
+// mod picker {
+//     use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+//     use windows::{runtime::*, Graphics::Capture::*, Win32::Foundation::*, Win32::UI::Shell::*};
+
+//     pub fn pick_picker<W: HasRawWindowHandle>(window: &W) -> Result<GraphicsCaptureItem> {
+//         let hwnd = match window.raw_window_handle() {
+//             RawWindowHandle::Windows(handle) => {
+//                 assert!(!handle.hwnd.is_null());
+//                 HWND(handle.hwnd as isize)
+//             }
+//             _ => panic!("Unsupported platform"),
+//         };
+
+//         let picker = GraphicsCapturePicker::new()?;
+//         let iw: IInitializeWithWindow = picker.cast()?;
+//         // SAFETY:
+//         // 1. hwnd is valid window handle and not null
+//         // 2. picker is valid
+//         unsafe { iw.Initialize(hwnd) }?;
+//         println!("Waiting for selection...");
+//         let result = picker.PickSingleItemAsync()?.get()?;
+//         println!("{:?}", result);
+
+//         Ok(result)
+//     }
+// }
+
+
+
+
---
+++ src/win.rs
@@ -0,0 +1,10 @@
+use windows::Win32::UI::WindowsAndMessaging::{SetWindowDisplayAffinity, WDA_EXCLUDEFROMCAPTURE};
+use windows::Win32::Foundation::HWND;
+use windows::runtime::Result;
+use winit::platform::windows::WindowExtWindows;
+use winit::window::Window;
+
+pub fn hide_from_capture(window: &Window) -> Result<()> {
+    let hwnd = window.hwnd();
+    unsafe{ SetWindowDisplayAffinity(HWND(hwnd as isize), WDA_EXCLUDEFROMCAPTURE) }.ok()
+}
